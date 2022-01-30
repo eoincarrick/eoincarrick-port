57792da9d124dac6a2570c90ed5e01803c918e23
Eoin Carrick <22carri99@gmail.com>
2022-01-30 18:55:08 +0000

    master

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,29 @@ function Navbar() {
   return (
     <div className="navbarContainer">
       <ul className="navbarListAndLink">
-        <div className="logoContainer"></div>
+        <div className="logoContainer">
+          <div className="e">E</div>
+
+          <div className="c">C</div>
+        </div>
+
         <div className="linksContainer">
           <Link to="/" className="links">
             Home
           </Link>
+
           <Link to="/projects" className="links">
             Projects
           </Link>
+
           <Link to="/photoshop_projects" className="links">
             PhotoShop Projects
           </Link>
+
           <Link to="/contact_me" className="links">
             Contact
           </Link>
+
           <Link to="/blog" className="links">
             Blog
           </Link>
