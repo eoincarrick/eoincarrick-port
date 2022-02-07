4aa9378e53ab556e426780529ed6d0ee3a0d1bec
Eoin Carrick <22carri99@gmail.com>
2022-02-07 21:16:59 +0000

    code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,26 @@ import { FaBars } from "react-icons/fa";
 import "./style/Nav.css";
 
 export const Nav = () => {
+  // const cancel = document.querySelector(".cancel");
+  // const nav = document.querySelector(".nav");
+  const btn = document.querySelector("#btn");
+  const menu = document.getElementsByClassName(".nav");
+
+  const remove = () => {
+    if (menu.style.display === "none") {
+      return (menu.style.display = "block");
+    } else {
+      return (menu.style.display = "none");
+    }
+  };
+
   return (
     <div className="navContainer">
       <div className="btn logo">
         <p className="logoName">eoincarrick</p>
-        <FaBars className="navIcon" />
+        <button onClick={this.remove} id="btn">
+          <FaBars className="navIcon" />
+        </button>
       </div>
       <nav className="nav">
         <Link className="link" to="/">
@@ -25,6 +40,7 @@ export const Nav = () => {
         <Link className="link" to="/contact-me">
           Contact
         </Link>
+        <span className="cancel">&#x2717;</span>
       </nav>
     </div>
   );
